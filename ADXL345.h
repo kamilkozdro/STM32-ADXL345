4bbc50a5a98c8211a84aa49145082c8a86faa71f
kamilkozdro <kamilkozdro@gmail.com>
2025-01-23 21:58:14 +0100

    Added basic adxl structs and functions prototypes

--- ADXL345.h
+++ ADXL345.h
@@ -33,13 +33,19 @@
 #define ADXL_REG_FIFO_STATUS			0x39	// FIFO status
 
 #define ADXL_BW_RATE_LOW_POWER_BIT		4		// 0 - Normal operation; 1 - Reduced power operation
-#define ADXL_BW_RATE_100_VAL			0x0A	// Default device bandwidth and output rate - 100 Hz
+#define ADXL_BW_RATE_100				0x0A	// Default device bandwidth and output rate - 100 Hz
+
+#define ADXL_POWER_MODE_NORMAL			0
+#define ADXL_POWER_MODE_LOW				1
 
 #define ADXL_POWER_CTL_LINK_BIT			5
 #define ADXL_POWER_CTL_AUTO_SLEEP_BIT	4
 #define ADXL_POWER_CTL_MEASURE_BIT		3		// Standby mode - 0; Measurement mode - 1
 #define ADXL_POWER_CTL_SLEEP_BIT		2
-#define ADXL_POWER_CTL_WAKEUP_8_VALUE	0		// 0 - 8Hz; 1 - 4Hz; 2 - 2Hz; 3 - 1Hz
+#define ADXL_POWER_CTL_WAKEUP_8_VALUE	0		// 8Hz
+#define ADXL_POWER_CTL_WAKEUP_4_VALUE	0		// 4Hz
+#define ADXL_POWER_CTL_WAKEUP_2_VALUE	0		// 2Hz
+#define ADXL_POWER_CTL_WAKEUP_1_VALUE	0		// 1Hz
 
 #define ADXL_INT_ENABLE_DATA_READY_BIT	7
 #define ADXL_INT_ENABLE_SINGLE_TAP_BIT	6
@@ -69,14 +75,52 @@
 #define ADXL_INT_SOURCE_OVERRUN_BIT		0
 
 #define ADXL_DATA_FORMAT_SELF_TEST_BIT	7
-#define ADXL_DATA_FORMAT_SPI_BIT		6	//	0 - 4-wire SPI mode; 1 - 3-wire SPI mode
+#define ADXL_DATA_FORMAT_SPI_BIT		6	// 0 - 4-wire SPI mode; 1 - 3-wire SPI mode
 #define ADXL_DATA_FORMAT_INT_INVERT_BIT	5	// 0 - Sets interrupts to active high; 1 - Interrupts active low
 #define ADXL_DATA_FORMAT_FULL_RES_BIT	3	// 0 - 10-bit mode, RANGE bits set max G range; 1 - resol. increases with G range to maintain 4 mG/LSB
 #define ADXL_DATA_FORMAT_JUSTIFY_BIT	2	// 0 - Right-justified mode with sign ext; 1 - Left-justified mode (MSB)
-#define ADXL_DATA_FORMAT_RANGE_2_VAL	0	// Range +/- 2G
-#define ADXL_DATA_FORMAT_RANGE_4_VAL	1	// Range +/- 4G
-#define ADXL_DATA_FORMAT_RANGE_8_VAL	2	// Range +/- 8G
-#define ADXL_DATA_FORMAT_RANGE_16_VAL	3	// Range +/- 16G
+#define ADXL_DATA_FORMAT_RANGE_2		0	// Range +/- 2G
+#define ADXL_DATA_FORMAT_RANGE_4		1	// Range +/- 4G
+#define ADXL_DATA_FORMAT_RANGE_8		2	// Range +/- 8G
+#define ADXL_DATA_FORMAT_RANGE_16		3	// Range +/- 16G
+
+#define ADXL_SPI_MODE_4WIRE				0
+#define ADXL_SPI_MODE_3WIRE				1
+
+#define ADXL_INT_INVERT_HIGH			0
+#define ADXL_INT_INVERT_LOW				1
+
+#define ADXL_RES_MODE_10BIT				0
+#define ADXL_RES_MODE_FULL				1
+
+#define ADXL_JUSTIFY_SIGNED				0
+#define ADXL_JUSTIFY_MSB				1
+
+
+typedef struct
+{
+	uint8_t spiMode;
+	uint8_t intInvert;
+	uint8_t resMode;
+	uint8_t justify;
+}ADXL_InitTypeDef;
+
+typedef struct
+{
+	GPIO_TypeDef* CS_Port;
+	uint32_t CS_Pin;
+	SPI_HandleTypeDef* hspi;
+}ADXL_Unit;
+
+void adxlWrite(uint8_t regAddr, uint8_t* dataToWrite);
+void adxlRead(uint8_t regAddr, uint8_t* readBuffer, size_t dataLength);
+void adxlInit();
+void adxlStandby();
+void adxlMeasure();
+void adxlSleep();
+
+
+
 
 
 
